b1e132bd77cba23d869e66a2285fa2e2282919b3
jrdnlx <jordansie13@gmail.com>
2022-02-25 17:52:54 -0500

    list placeholders

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,4 +1,12 @@
 import React from "react";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 const Movies = () => {
   const [arr, addArray] = React.useState([]);
 
@@ -12,17 +20,33 @@ const Movies = () => {
   };
 
   return (
-    <div>
-      <button onClick={Add}>Create List</button>
+    <Grid container spacing={2}>
+      <Grid xs={8}></Grid>
+      <Grid xs={4}>
+        <Fab onClick={Add} variant="extended">
+          <AddIcon sx={{ mr: 1 }} />
+          Add
+        </Fab>
+      </Grid>
+
       {arr.map((data) => (
-        <div key={data.id}>
-          <h2>{data.title}</h2>
-          <table></table>
-        </div>
+        <Grid item key={data.id} xs={12} md={4}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h2" component="div">
+                  {data.title}
+                </Typography>
+                <Typography variant="body" color="text.secondary">
+                  <textarea rows={8} cols={12}></textarea>
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 };
 
-const MovieList = (props) => {};
 export default Movies;
