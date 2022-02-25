3c7cd8d20e57e1f72c67bc369972d9580a38a6e3
jrdnlx <jordansie13@gmail.com>
2022-02-25 16:35:05 -0500

    Creating multiple lists

--- noteboard/src/components/Movies.js
+++ noteboard/src/components/Movies.js
@@ -1,12 +1,28 @@
+import React from "react";
 const Movies = () => {
-  const create = () => {
+  const [arr, addArray] = React.useState([]);
+
+  const Add = () => {
     console.log("Clicked");
+    addArray((list) => [
+      ...list,
+      { title: `List ${list.length + 1}`, id: list.length + 1 },
+    ]);
+    console.log(arr);
   };
 
   return (
     <div>
-      <button onClick={create}>Create List</button>
+      <button onClick={Add}>Create List</button>
+      {arr.map((data) => (
+        <div key={data.id}>
+          <h2>{data.title}</h2>
+          <table></table>
+        </div>
+      ))}
     </div>
   );
 };
+
+const MovieList = (props) => {};
 export default Movies;
