fa2e6d9adc29ddc05e4eba6d2cc6e554b92d26f0
jrdnlx <jordansie13@gmail.com>
2022-06-24 02:37:32 -0400

    remove card media

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,6 @@ import AddIcon from "@mui/icons-material/Add";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 const Movies = () => {
